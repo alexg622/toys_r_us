b0633e61c45d63c5f6be49e230902b1874ca7607
alexg622 <alex.gm62288@gmail.com>
2018-08-17 01:57:59 -0700

    Make better

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken')
 const users = require('./routes/api/users')
 const toys = require('./routes/api/toys')
 const db = require('./config/keys').mongoURI
+const path = require('path')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
@@ -20,6 +21,13 @@ app.get('/test', (req, res) => res.send("working"))
 
 app.use('/api/users', users)
 app.use('/api/toys', toys)
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 
